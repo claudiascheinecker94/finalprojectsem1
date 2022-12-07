c415ad54d6eb99e3d2c6b92c7fad25157c8ffedb
claudiascheinecker94 <116496696+claudiascheinecker94@users.noreply.github.com>
2022-12-07 20:30:38 +0000

    Consolidation of webpages

---
+++ from others/Project-Update/Project/Script.js
@@ -0,0 +1,10 @@
+function newsletter(){
+    var input = document.getElementById('input').value;
+    var button = document.getElementById('button-addon2').value;
+
+    if(button.clicked = 'true' && input != ''){
+    alert("You have successfully signed up for our newsletter! Thanks a lot!");
+    } else {
+        alert("Please add an email address to sign up!");
+    }
+}
