7036cb965f74915eb61772e0a8445133d10bbd21
claudiascheinecker94 <116496696+claudiascheinecker94@users.noreply.github.com>
2022-12-13 21:36:05 +0000

    Final update

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -1,3 +1,4 @@
+//Code by Claudia
 function newsletter(){
     var input = document.getElementById('input').value;
     var button = document.getElementById('button-addon2').value;
@@ -12,3 +13,36 @@ function newsletter(){
         alert("Please add an email address to sign up!");
         }
 }
+//Code by Kevin
+function showDescription(id){
+
+    document.getElementById(id+"Info").style.display="block";
+    document.getElementById(id+"DownArrow").style.visibility="hidden";
+    document.getElementById(id+"UpArrow").style.display="block";
+    
+    
+}
+//Code by Kevin
+function hideDescription(id){
+    document.getElementById(id+"Info").style.display="none";
+    document.getElementById(id+"DownArrow").style.visibility="visible";
+    document.getElementById(id+"UpArrow").style.display="none";
+
+}
+// Code by David
+function validateForm() {
+	var name=document.myForm.name.value;
+	var email=document.myForm.email.value;
+	var message=document.myForm.message.value;
+	
+	if(name==""){
+		alert("Please Enter Name");
+		return false;
+	}
+	else if(email==""){
+		alert("Please Enter Email");
+		return false;
+	}
+	
+}
+
